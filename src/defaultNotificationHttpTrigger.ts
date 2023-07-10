b3a00ed5d822a5f8d55f9cdc6b0b8d890fc16f36
Sanjeev Kumar <sanjeevscet@gmail.com>
2023-07-10 09:33:01 +0530

    Adding color update code on the basis of priority

--- src/defaultNotificationHttpTrigger.ts
+++ src/defaultNotificationHttpTrigger.ts
@@ -22,6 +22,15 @@ const httpTrigger: AzureFunction = async function (
   // if (name) {
   //   data.title = name;
   // }
+  const priority = +req.body?.priority;
+  if (priority > 2) {
+    notificationTemplate.body[0].color = "warning";
+  }
+  else if (priority === 1 || priority === 2) {
+    notificationTemplate.body[0].color = "good";
+  } else {
+    notificationTemplate.body[0].color = "dark";
+  }
   for (const target of await notificationApp.notification.installations()) {
     await target.sendAdaptiveCard(
       AdaptiveCards.declare<CardData>(notificationTemplate).render(postedData)
